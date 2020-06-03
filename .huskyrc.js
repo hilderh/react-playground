2cf4c5185556032221b39e965766e5488830681b
hilderdev <hhernandez@acid.cl>
2020-06-03 10:05:51 -0400

    feat(hh): add husky config

--- .huskyrc.js
+++ .huskyrc.js
@@ -2,6 +2,6 @@ module.exports = {
   hooks: {
     'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
     'pre-commit': 'yarn run lint-staged',
-    'pre-push': 'yarn run code:check',
+    'pre-push': 'yarn code:check',
   },
 };
